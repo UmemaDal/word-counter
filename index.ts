6cb603120e77df811c3f2e4cd429255c6f597cb1
UmemaDal <115360897+UmemaDal@users.noreply.github.com>
2023-01-25 11:18:07 -0800

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+
+const answers:{
+    sentence:string
+} = await inquirer.prompt([
+    {
+        name:"sentence",
+        type:"input",
+        message:"enter your sentence to find the word "
+    }
+])
+const words=answers.sentence.trim().split(" ")
+console.log(`your sentence word count is ${words.length}`)
+
+
+
+
